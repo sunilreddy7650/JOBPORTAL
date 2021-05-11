fbf35926056a45ef7ee4f9495d65fc79e580f553
sunilreddy7650 <62066605+sunilreddy7650@users.noreply.github.com>
2021-05-11 21:37:15 +0530

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,43 @@
+<!-- Main menu (tabs) -->
+<br>
+
+<link href="themes/8/js-image-slider.css" rel="stylesheet" type="text/css" />
+    <script src="themes/8/js-image-slider.js" type="text/javascript"></script>
+    <link href="themes/8/tooltip.css" rel="stylesheet" type="text/css" />
+    <script src="themes/8/tooltip.js" type="text/javascript"></script>
+	
+	
+	<div id="sliderFrame">
+        <div id="slider">
+
+            <img src="images1/image-slider-1.jpg" alt="#cap1" />
+            <img src="images1/image-slider-2.jpg" alt="Welcome to Online Job Portal" />
+            <img src="images1/image-slider-3.jpg" alt="Welcome to Online Job Portal" />
+            <img src="images1/image-slider-4.jpg" alt="#cap2" />
+            <img src="images1/image-slider-5.jpg" alt="Welcome to Online Job Portal" />
+        </div>
+        <div style="display: none;">
+            <div id="cap1">
+                Welcome to Online Job Portal
+            </div>
+            <div id="cap2">
+                Good Services!!!
+            </div>
+        </div>
+    </div>
+	<br><br>
+	
+     <div id="tabs" class="noprint">
+
+            <h3 class="noscreen">Navigation</h3>
+            <ul class="box">
+                <li><a href="index.php">Home<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="AboutUs.php">About Us<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="Employer.php">Employer<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                 <li><a href="JobSeeker.php">Job Seeker<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                  <li><a href="News.php">Latest News<span class="tab-l"></span><span class="tab-r"></span></a></li>
+                <li><a href="ContactUs.php">Contact Us<span class="tab-l"></span><span class="tab-r"></span></a></li>
+            </ul>
+
+        <hr class="noscreen" />
+     </div> <!-- /tabs -->
